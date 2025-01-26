4e0b27c7d422a3a106d3e99d38b97ccc78105add
Alexander_Tirsky <tirsky06@gmail.com>
2025-01-26 12:51:14 +1000

    add structure file

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// const intro = document.getElementsByClassName("intro");
+
+// console.log(intro);
+
+// const btnTesrEvent = document.getElementById("btn-test-event");
+// console.log(btnTesrEvent);
+
+
+// function viewEvent(e) {
+//     const event = e;
+//     console.log(event);
+    
+// }
+
+// btnTesrEvent.addEventListener("click", viewEvent);
+
+// console.log(window);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+// console.log(navigator.userAgent);
+
